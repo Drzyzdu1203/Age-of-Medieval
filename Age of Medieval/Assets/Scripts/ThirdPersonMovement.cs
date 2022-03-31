74619831bba65db38f8fb971226ec0f2e5d4ac4a
Mateusz Drzyzga <mdrzyzdu@gmail.com>
2022-03-31 12:03:44 +0200

    update camera script

--- Age of Medieval/Assets/Scripts/ThirdPersonMovement.cs
+++ Age of Medieval/Assets/Scripts/ThirdPersonMovement.cs
@@ -7,6 +7,8 @@ public class ThirdPersonMovement : MonoBehaviour
     public CharacterController controller;
 
     public float speed = 6f;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
 
     // Update is called once per frame
     void Update()
@@ -17,6 +19,10 @@ public class ThirdPersonMovement : MonoBehaviour
 
         if(direction.magnitude >= 0.1f)
         {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
             controller.Move(direction * speed * Time.deltaTime);
         }
     }
