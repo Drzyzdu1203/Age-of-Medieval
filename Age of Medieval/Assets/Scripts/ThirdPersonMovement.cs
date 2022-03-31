0b573ae4c9182ab4c332f179c7ba437dbb2e18c2
Mateusz Drzyzga <mdrzyzdu@gmail.com>
2022-03-31 12:28:33 +0200

    fix camera script

--- Age of Medieval/Assets/Scripts/ThirdPersonMovement.cs
+++ Age of Medieval/Assets/Scripts/ThirdPersonMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ThirdPersonMovement : MonoBehaviour
 {
     public CharacterController controller;
+    public Transform cam;
 
     public float speed = 6f;
     public float turnSmoothTime = 0.1f;
@@ -19,11 +20,12 @@ public class ThirdPersonMovement : MonoBehaviour
 
         if(direction.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-            controller.Move(direction * speed * Time.deltaTime);
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
     }
 }
