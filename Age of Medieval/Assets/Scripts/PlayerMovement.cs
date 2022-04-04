78489bc856a070d6f24d97a29d0e039de80f31d3
Mateusz Drzyzga <mdrzyzdu@gmail.com>
2022-04-04 23:06:48 +0200

    replace moving script
    add better - simple script for moving & running

--- Age of Medieval/Assets/Scripts/PlayerMovement.cs
+++ Age of Medieval/Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,12 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float runSpeed;
 
     private Vector3 moveDirection;
+    private Vector3 velocity;
+
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private float gravity;
 
     private CharacterController controller;
 
@@ -23,11 +29,51 @@ public class PlayerMovement : MonoBehaviour
     }
     private void Move()
     {
+        isGrounded = Physics.CheckSphere(transform.position, groundCheckDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -5f;
+        }
+
         float moveZ = Input.GetAxis("Vertical");
 
         moveDirection = new Vector3(0, 0, moveZ);
-        moveDirection *= walkSpeed;
+       
+        if (isGrounded)
+        {
+            if (moveDirection != Vector3.zero && !Input.GetKey(KeyCode.LeftShift))
+            {
+                Walk();
+            }
+            else if (moveDirection != Vector3.zero && Input.GetKey(KeyCode.LeftShift))
+            {
+                Run();
+            }
+            else if (moveDirection == Vector3.zero)
+            {
+                Idle();
+            }
+
+            moveDirection *= moveSpeed;
+        }
 
         controller.Move(moveDirection * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+    
+    private void Idle()
+    {
+
+    }
+    private void Walk()
+    {
+        moveSpeed = walkSpeed;
+    }
+    private void Run()
+    {
+        moveSpeed = runSpeed;
     }
 }
