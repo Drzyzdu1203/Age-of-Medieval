7855b4616461f7f8fad0661e817a0d8424d9f69d
Your Name <pstruzik7@wp.pl>
2022-05-13 15:18:11 +0200

    adding loading bar

---
+++ Age of Medieval/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] private GameObject progressPanel;
+    [SerializeField] private Slider progressBar;
+    [SerializeField] private Text progressValue;
+
+
+    public void Awake()
+    {
+        progressPanel.SetActive(false);
+
+    }
+
+    public void LoadLevel(string levelName)
+    {
+        progressPanel.SetActive(true);
+
+        StartCoroutine(LoadLevelAsync(levelName));
+    }
+
+    private IEnumerator LoadLevelAsync(string levelName)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(levelName);
+
+        while(!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress);
+
+            progressBar.value = progress;
+            progressValue.text = (progress * 100f).ToString("F0") + "%";
+
+           // Debug.Log(progress);
+
+            yield return null;
+        }
+    }
+}
