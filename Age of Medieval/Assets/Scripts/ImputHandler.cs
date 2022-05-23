9229566e74382b50b666e297aaaf6b7c529962ff
Mateusz Drzyzga <mdrzyzdu@gmail.com>
2022-05-23 10:20:38 +0200

    add basic ImputHandler script

--- Age of Medieval/Assets/Scripts/ImputHandler.cs
+++ Age of Medieval/Assets/Scripts/ImputHandler.cs
@@ -2,17 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ImputHandler : MonoBehaviour
+namespace AoM
 {
-    // Start is called before the first frame update
-    void Start()
+    public class ImputHandler : MonoBehaviour
     {
-        
-    }
+        public float horizontal;
+        public float vertical;
+        public float moveAmount;
+        public float mouseX;
+        public float mouseY;
 
-    // Update is called once per frame
-    void Update()
-    {
+        PlayerControls inputActions;
+
+        Vector2 movementInput;
+        Vector2 cameraInput;
+
+        public void OnEnable()
+        {
+            if (inputActions == null)
+            {
+                inputActions = new PlayerControls();
+                inputActions.PlayerMovement.Movement.performed += inputActions => movementInput = inputActions.ReadValue<Vector2>();
+                inputActions.PlayerMovement.Camera.performed += i => cameraInput = i.ReadValue<Vector2>();
+            }
+            inputActions.Enable();
+        }
+        private void OnDisable()
+        {
+            inputActions.Disable();
+        }
+
+        public void TickInput(float delta)
+        {
+            MoveInput(delta);
+        }
+
+        private void MoveInput(float delta)
+        {
+            horizontal = movementInput.x;
+            vertical = movementInput.y;
+            moveAmount = Mathf.Clamp01(Mathf.Abs(horizontal) + Mathf.Abs(vertical));
+            mouseX = cameraInput.x;
+            mouseY = cameraInput.y;
+                
+        }
         
     }
 }
