99ccc4d9ef321683cb90a3576c4b406319060273
PawelSzczepaniak99 <61648162+PawelSzczepaniak99@users.noreply.github.com>
2022-07-04 00:02:42 +0200

    Vilage

---
+++ Age of Medieval/Assets/Scripts/Movement Scripts/PlayerManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Script2
+{
+    public class PlayerManager : MonoBehaviour
+    {
+        InputHandler inputHandler;
+        Animator anim;
+        // Start is called before the first frame update
+        void Start()
+        {
+            inputHandler = GetComponent<InputHandler>();
+            anim = GetComponentInChildren<Animator>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            inputHandler.isinteracting = anim.GetBool("isinteracting");
+            inputHandler.rollFlag = false;
+            inputHandler.sprintFlag = false;
+        }
+    }
+}
