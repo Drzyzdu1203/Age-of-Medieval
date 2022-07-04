853df7cdf17f0b7e76b0da5b9956c1119dff08a3
Mateusz Drzyzga <mdrzyzdu@gmail.com>
2022-07-04 15:23:38 +0200

    cleaning project structure

---
+++ Age of Medieval/Assets/Scripts/Managers/PlayerManager.cs
@@ -0,0 +1,147 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AoM
+{
+    public class PlayerManager : CharacterManager
+    {
+        InputHandler inputHandler;
+        Animator anim;
+        CameraHandler cameraHandler;
+        PlayerStats playerStats;
+        PlayerAnimatorManager playerAnimatorManager;
+        PlayerLocomotion playerLocomotion;
+
+        InteractableUI interactableUI;
+        public GameObject interactableUIGameObject;
+        public GameObject itemInteractableGameObject;
+
+        public bool isinteracting;
+
+        [Header("Player Flags")]
+        public bool isSprinting;
+        public bool isInAir;
+        public bool isGrounded;
+        public bool canDoCombo;
+        public bool isUsingRightHand;
+        public bool isUsingLeftHand;
+        public bool isInvulerable;
+
+        private void Awake()
+        {
+            cameraHandler = FindObjectOfType<CameraHandler>();
+            backStabCollider = GetComponentInChildren<BackStabCollider>();
+            inputHandler = GetComponent<InputHandler>();
+            playerAnimatorManager = GetComponentInChildren<PlayerAnimatorManager>();
+            anim = GetComponentInChildren<Animator>();
+            playerStats = GetComponent<PlayerStats>();
+            playerLocomotion = GetComponent<PlayerLocomotion>();
+            interactableUI = FindObjectOfType<InteractableUI>();
+        }
+
+        void Update()
+        {
+            float delta = Time.deltaTime;
+
+            isinteracting = anim.GetBool("isinteracting");
+            canDoCombo = anim.GetBool("canDoCombo");
+            isUsingRightHand = anim.GetBool("isUsingRightHand");
+            isUsingLeftHand = anim.GetBool("isUsingLeftHand");
+            isInvulerable = anim.GetBool("isInvulnerable");
+            anim.SetBool("isInAir", isInAir);
+            anim.SetBool("isDead", playerStats.isDead);
+
+            inputHandler.TickInput(delta);
+            playerAnimatorManager.canRotate = anim.GetBool("canRotate");
+            playerLocomotion.HandleRollingAndSprinting(delta);
+            playerLocomotion.HandleJumping();
+            playerStats.RegenerateStamina();
+            
+            CheckForInteractableObject();
+        }
+
+        private void FixedUpdate()
+        {
+            float delta = Time.fixedDeltaTime;
+
+            playerLocomotion.HandleFalling(delta, playerLocomotion.moveDirection);
+            playerLocomotion.HandleMovement(delta);
+            playerLocomotion.HandleRotation(delta);
+        }
+        private void LateUpdate()
+        {
+            inputHandler.rollFlag = false;
+            inputHandler.lightAttack_Input = false;
+            inputHandler.heavyAttack_Input = false;
+            inputHandler.d_Pad_Up = false;
+            inputHandler.d_Pad_Down = false;
+            inputHandler.d_Pad_Left = false;
+            inputHandler.d_Pad_Right = false;
+            inputHandler.interaction_Input = false;
+            inputHandler.jump_Input = false;
+            inputHandler.inventory_Input = false;
+
+            float delta = Time.deltaTime;
+
+            if (cameraHandler != null)
+            {
+                cameraHandler.FollowTarget(delta);
+                cameraHandler.HandleCameraRotation(delta, inputHandler.mouseX, inputHandler.mouseY);
+            }
+
+            if (isInAir)
+            {
+                playerLocomotion.inAirTimer = playerLocomotion.inAirTimer + Time.deltaTime;
+            }
+        }
+
+        #region Player Interactions
+
+        public void CheckForInteractableObject()
+        {
+            RaycastHit hit;
+
+            if (Physics.SphereCast(transform.position, 0.3f, transform.forward, out hit, 1f))
+            {
+                if (hit.collider.tag == "Interactable")
+                {
+                    Interactable interactableObject = hit.collider.GetComponent<Interactable>();
+
+                    if (interactableObject != null)
+                    {
+                        string interactableText = interactableObject.interactbleText;
+                        interactableUI.interactableText.text = interactableText;
+                        interactableUIGameObject.SetActive(true);
+
+                        if (inputHandler.interaction_Input)
+                        {
+                            hit.collider.GetComponent<Interactable>().Interact(this);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (interactableUIGameObject != null)
+                {
+                    interactableUIGameObject.SetActive(false);
+                }
+
+                if (itemInteractableGameObject != null && inputHandler.interaction_Input)
+                {
+                    itemInteractableGameObject.SetActive(false);
+                }
+            }
+        }
+
+        public void OpenChestInteraction(Transform playerStandsHereWhenOpeningChest)
+        {
+            playerLocomotion.rigidbody.velocity = Vector3.zero;
+            transform.position = playerStandsHereWhenOpeningChest.transform.position;
+            playerAnimatorManager.PlayTargetAnimation("Gathering", true);
+        }
+
+        #endregion
+    }
+}
---
+++ Age of Medieval/Assets/Scripts/Managers/WeaponSlotManager.cs
@@ -0,0 +1,142 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AoM
+{
+    public class WeaponSlotManager : MonoBehaviour
+    {
+        PlayerManager playerManager;
+        PlayerInventory playerInventory;
+
+        public WeaponItem attackingWeapon;
+
+        public WeaponHolderSlot leftHandSlot;
+        public WeaponHolderSlot rightHandSlot;
+        public WeaponHolderSlot backSlot;
+
+        public DamageCollider leftHandDamageCollider;
+        public DamageCollider rightHandDamageCollider;
+
+        Animator animator;
+
+        QuickSlotsUI quickSlotsUI;
+
+        PlayerStats playerStats;
+        InputHandler inputHandler;
+
+        private void Awake()
+        {
+            playerManager = GetComponentInParent<PlayerManager>();
+            playerInventory = GetComponentInParent<PlayerInventory>();
+            animator = GetComponent<Animator>();
+            quickSlotsUI = FindObjectOfType<QuickSlotsUI>();
+            playerStats = GetComponentInParent<PlayerStats>();
+            inputHandler = GetComponentInParent<InputHandler>();
+
+            WeaponHolderSlot[] weaponHolderSlots = GetComponentsInChildren<WeaponHolderSlot>();
+            foreach (WeaponHolderSlot weaponSlot in weaponHolderSlots)
+            {
+                if (weaponSlot.isLeftHandSlot)
+                {
+                    leftHandSlot = weaponSlot;
+                }
+                else if (weaponSlot.isRightHandSlot)
+                {
+                    rightHandSlot = weaponSlot;
+                }
+                else if (weaponSlot.isBackSlot)
+                {
+                    backSlot = weaponSlot;
+                }
+            }
+        }
+
+        public void LoadBothWeaponsOnSlots()
+        {
+            LoadWeaponOnSlot(playerInventory.rightWeapon, false);
+            LoadWeaponOnSlot(playerInventory.leftWeapon, true);
+        }
+
+        public void LoadWeaponOnSlot(WeaponItem weaponItem, bool isLeft)
+        {
+            if (isLeft)
+            {
+                leftHandSlot.currentWeapon = weaponItem;
+                leftHandSlot.LoadWeaponModel(weaponItem);
+                LoadLeftWeaponDamageCollider();
+                quickSlotsUI.UpdateWeaponQuickSlotsUI(true, weaponItem);
+            }
+            else
+            {
+                if (inputHandler.twoHandFlag)
+                {
+                    
+                    animator.Play("Get Shield");
+                        
+                    
+                    backSlot.LoadWeaponModel(leftHandSlot.currentWeapon);
+                    leftHandSlot.UnloadWeaponAndDestroy();
+                    animator.CrossFade(weaponItem.th_idle, 0.2f);
+                }
+                else
+                {
+                    animator.Play("Get Shield");
+                    animator.CrossFade("Both Arms Empty", 0.1f);
+                    backSlot.UnloadWeaponAndDestroy();
+                }
+
+                rightHandSlot.currentWeapon = weaponItem;
+                rightHandSlot.LoadWeaponModel(weaponItem);
+                LoadRightWeaponDamageCollider();
+                quickSlotsUI.UpdateWeaponQuickSlotsUI(false, weaponItem);
+                
+            }
+        }
+
+        #region Handle Weapon's Damage Collider
+
+        private void LoadLeftWeaponDamageCollider()
+        {
+            leftHandDamageCollider = leftHandSlot.currentWeaponModel.GetComponentInChildren<DamageCollider>();
+        }
+
+        private void LoadRightWeaponDamageCollider()
+        {
+            rightHandDamageCollider = rightHandSlot.currentWeaponModel.GetComponentInChildren<DamageCollider>();
+        }
+
+        public void OpenDamageCollider()
+        {
+            if (playerManager.isUsingRightHand)
+            {
+                rightHandDamageCollider.EnableDamageCollider();
+            }
+            else if(playerManager.isUsingLeftHand)
+            {
+                leftHandDamageCollider.EnableDamageCollider();
+            }
+        }
+
+        public void CloseDamageCollider()
+        {
+            rightHandDamageCollider.DisaleDamageCollider();
+            //leftHandDamageCollider.DisaleDamageCollider();
+        }
+
+        #endregion
+
+        #region Handle Weapon's Stamina Drainage
+        public void DrainStaminaLightAttack()
+        {
+            playerStats.TakeStaminaDamage(Mathf.RoundToInt(attackingWeapon.baseStamina * attackingWeapon.lightAttackMultiplier));
+            Debug.Log("DrainStaminaLightAttack");
+        }
+
+        public void DrainStaminaHeavyAttack()
+        {
+            playerStats.TakeStaminaDamage(Mathf.RoundToInt(attackingWeapon.baseStamina * attackingWeapon.heavyAttackMultiplier));
+        }
+        #endregion
+    }
+}
