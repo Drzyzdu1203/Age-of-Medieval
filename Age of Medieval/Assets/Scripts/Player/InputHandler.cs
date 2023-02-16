2e6d6bdea14ce3816e0cbfb4b13872fc2ff8d052
Mateusz Drzyzga <47924540+Drzyzdu1203@users.noreply.github.com>
2023-02-16 19:44:34 +0100

    Version 1.0
    ready final wersion 1.0

--- Age of Medieval/Assets/Scripts/Player/InputHandler.cs
+++ Age of Medieval/Assets/Scripts/Player/InputHandler.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEngine.InputManagerEntry;
+//using static UnityEngine.InputManagerEntry;
 
 namespace AoM
 {
